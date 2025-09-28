49a117986fa81531b71f3efa0ce7c40feffcb317
ROSEZELLA01 <obatimehinroseline@gmail.com>
2025-09-28 12:00:29 +0100

    completed simple calculator project

--- Simple Calculator/script.js
+++ Simple Calculator/script.js
@@ -1,6 +1,7 @@
 const display = document.querySelector('#input');
 const buttons = document.querySelectorAll(".calc-button");
 const buttonEqual = document.querySelector("#button-equals");
+const buttonClear = document.querySelector("#button-clear");
 buttons.forEach(button => {
     button.addEventListener("click", () => {
         display.value += button.textContent;
@@ -16,12 +17,21 @@ buttonEqual.addEventListener("click", () => {
     }
 });
 
+buttonClear.addEventListener("click", () => {
+display.value=""
+})
+
 display.addEventListener("keydown", (event) => {
     if (event.key === "Enter") {
-        try {
-            display.value = eval(display.value);
-        } catch (error) {
-            display.value = "Error";
-        }
+      display.value = eval(display.value);
+    } else {
+      display.value = key;
     }
+    // if (event.key === "Enter") {
+    //     try {
+    //         display.value = eval(display.value);
+    //     } catch (error) {
+    //         display.value = "Error";
+    //     }
+    // }
 });
